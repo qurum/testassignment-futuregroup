0aaa6236619e2f699f6aca05528dad18361e4839
roni.rebane <qurum@yandex.ru>
2023-07-30 06:17:00 +0300

    fix: test.sh fix: install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 cp ./back/.env.example ./back/.env
 cp ./back/.test.env.example ./back/tests/.env
 docker-compose up -d
-docker exec testassignment_php_fpm chown -R www-data:www-data /database
 docker exec testassignment_php_fpm composer install
 docker exec testassignment_php_fpm php /app/vendor/bin/phinx migrate -e development
-docker-compose down
+docker exec testassignment_php_fpm chown -R www-data:www-data /database
 echo "---------"
 echo "All done."
--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 docker-compose -f docker-compose.test.yml up -d --remove-orphans
-docker exec testassignment_php_fpm_test chown -R www-data:www-data /database
-docker exec testassignment_php_fpm_test php /app/vendor/bin/phinx migrate -e development
+docker exec -u root testassignment_php_fpm_test php /app/vendor/bin/phinx migrate -e development
+docker exec -u root testassignment_php_fpm_test chown -R www-data:www-data /database
 docker exec testassignment_php_fpm_test php /app/vendor/bin/codecept run --steps Unit
 docker exec testassignment_php_fpm_test php /app/vendor/bin/codecept run --steps Api
 docker-compose down
